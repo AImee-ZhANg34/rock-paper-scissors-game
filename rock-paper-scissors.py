84c79b36bbd9e49eeb7dfb9c290deab4a444b8e8
Ishmael <jiajieleiwork52@gmail.com>
2025-07-31 22:57:16 +0000

    Create rock-paper-scissors.py
    Don't put code in markdown(.md) files. Create a separate python(.py) script. Also consider using match statements to improve readability. https://www.w3schools.com/python/python_match.asp

---
+++ rock-paper-scissors.py
@@ -0,0 +1,28 @@
+import sys
+
+valid_choices = ["rock", "paper", "scissors"]
+
+player1 = input("Rock, paper, or scissors?").lower()
+player2 = input("Rock, paper, or scissors?").lower()
+
+def win (u1, u2): 
+  if(u1 == u2): 
+    return("Draw") 
+  elif(u1 == "rock"): 
+    if(u2 == "scissors"): 
+      print("Player 1 wins")
+    else: 
+      print("Player 2 wins") 
+  elif(u1 == "paper"):
+    if (u2 == "rock"): 
+      print("Player 1 wins") 
+    else: 
+      print("Player 2 wins") 
+  elif(u1 == "scissors"): 
+    if (u2 == "paper"): 
+      print("Player 1 wins") 
+    else: 
+      print("Player 2 wins") 
+  else: return("replay game") sys.exit()
+
+win(player1, player2)
